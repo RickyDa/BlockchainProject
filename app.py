b6ed6ac437341bd8636ec30436b069364a19c4b4
RickyDanipog <rickyd@mail.afeka.ac.il>
2020-08-20 15:34:03 +0300

    finished leader election

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 from flask import Flask
 from flask import request, Response
+from utils import *
+import requests
 import user_controller as uc
 
+PORT = 5000
 app = Flask(__name__)
 
 
@@ -30,15 +33,81 @@ def login():
     return Response(status=401)
 
 
+"""
+############################# LEADER ELECTION ###################################
+### The leader is the one the get not responses OR has the highest rank (ID)  ###
+https://www.cs.colostate.edu/~cs551/CourseNotes/Synchronization/BullyExample.html
+"""
+ID = get_instance_ID()
+LEADER_ID = ''
+LEADER_DNS = ''
+
+
+@app.route('/getleader', methods=['GET'])
+def get_leader():
+    return {'LEADER_ID': str(LEADER_ID), 'LEADER_DNS': str(LEADER_DNS)}
+
+
+@app.route('/leader', methods=['POST'])
+def assign_leader():
+    global LEADER_ID, LEADER_DNS
+    imd = request.form
+    LEADER_ID = imd['leader']
+    LEADER_DNS = imd['PUBLIC_DNS']
+    print(f"The Leader is: ID - {LEADER_ID} on {LEADER_DNS}")
+    return Response(status=200)
+
+
+def broadcast(nodes, path, payload):
+    response_count = 0
+
+    for node in nodes:
+        try:
+            if path == 'leader':
+                requests.post(f"http://{node['DNS']}:{PORT}/{path}", data=payload)
+            else:
+                requests.get(f"http://{node['DNS']}:{PORT}/{path}", data=payload)
+            response_count += 1
+        except Exception as e:
+            print(e)
+    return response_count
+
+
+@app.route('/elect', methods=['GET', 'POST'])
+def elect():
+    """
+    starts bully leader election between nodes
+    """
+    if request.method == 'GET':
+        nodes = get_instances()
+        higher_ranks = find_higher_ranks(nodes=nodes, my_rank=ID)
+        if len(higher_ranks) == 0:
+            print('No higher ranks Im the leader')
+            broadcast(nodes=nodes, path="leader", payload={'leader': ID,
+                                                           'PUBLIC_DNS': get_public_DNS()})
+        else:
+            print('Wait for responses')
+            response_count = broadcast(nodes=higher_ranks, path="elect", payload={'leader': ID})
+            if response_count == 0:
+                print('No responses Im the leader')
+                broadcast(nodes=nodes, path="leader", payload={'leader': ID,
+                                                               'PUBLIC_DNS': get_public_DNS()})
+    return Response(status=200)
+
+
+"""
+############################# LEADER ELECTION ###################################
+"""
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0', port=PORT, threaded=True)  # on ec2 host='0.0.0.0', on local host='localhost'
 
 # APP
-# Leader Election - ricky
-# Client side - login/ register, User API- dorel
-# Transaction API - itay
-# # Client side - transcation
-# master
+# Leader Election - ricky Done.
+# TODO:
+#   Client side - login/ register, User API- dorel
+#   Transaction API - itay
+#   Client side - transcation
+#   master
 
 
 # Create user
@@ -52,7 +121,7 @@ if __name__ == '__main__':
 }
 '''
 
-#login
+# login
 '''
 {
     "user_email" : "ricky@gmail.com",
---
+++ utils.py
@@ -0,0 +1,51 @@
+import subprocess
+import boto3
+
+
+def get_public_DNS():
+    """
+
+    :return: return public DNS of this local instance
+    """
+    cmd = ['wget', '-qO-', 'http://instance-data/latest/meta-data/public-ipv4/']
+    proc = subprocess.Popen(cmd, stdout=subprocess.PIPE,
+                            stderr=subprocess.PIPE)
+
+    o, _ = proc.communicate()
+    return o.decode('ascii')
+
+
+def get_instance_ID():
+    """
+    computes instance ID(sum of ip digits) for leader election purpose
+
+    :return: the id of the instance (sum of ip digits)
+    """
+    # TODO ID is not atomic
+    p_ip = get_public_DNS()
+    return str(sum([int(x) for x in p_ip.split('.')]))
+
+
+def get_instances():
+    instances = []
+    ec2 = boto3.client('ec2')
+    response = ec2.describe_instances()
+
+    for res in response['Reservations']:
+        for inst in res['Instances']:
+            print(inst['PublicDnsName'])
+            print(inst['PublicIpAddress'])
+            instances.append({'DNS': inst['PublicDnsName'],
+                              'Adress': inst['PublicIpAddress'],
+                              'ID': str(sum([int(x) for x in inst['PublicIpAddress'].split('.')]))
+                              })
+    return instances
+
+
+def find_higher_ranks(nodes, my_rank):
+    rv = []
+    for node in nodes:
+        if int(node['ID']) > int(my_rank):
+            rv.append(node)
+
+    return rv
