10dbb1091237ab33331f6b06390edb758e868752
RickyDanipog <rickyd@mail.afeka.ac.il>
2020-08-30 14:31:00 +0300

    added 5 instances creation

--- setup.py
+++ setup.py
@@ -1,6 +1,5 @@
 import boto3
 from botocore.exceptions import ClientError
-from globals import cfg
 
 """
 INITIALIZE: BLOCKCHAIN NETWORK
@@ -8,7 +7,20 @@ INITIALIZE: BLOCKCHAIN NETWORK
 - 2 dynamodb
 - 1 SQS 
 - 1 S3 BUCKET
+- 5 INSTANCES
 """
+
+PORT = 5000
+TRANSACTION_LIMIT = 20
+Q_NAME = 'transactions'
+BUCKET_NAME = 'dsblocks'
+TRANSACTION_TABLE = 'transactions'
+USER_TABLE = 'users'
+
+NUM_INSTANCES = 5
+IMAGE_ID = "ami-04cfbd64f9364a31f"
+KEY = "DSKey"
+SECURITY_GROUPS = ["WAID_testing"]
 
 
 def create_db(table_name, keys):
@@ -54,19 +66,39 @@ def create_bucket(bucket_name, region=None):
     return True
 
 
+def create_instances(num_instances, image_id, key_pem, sg):
+    ec2 = boto3.resource('ec2')
+
+    try:
+        ec2.create_instances(
+            ImageId=image_id,
+            MinCount=num_instances,  # [Optional] Minimum instances to launch
+            MaxCount=num_instances,  # [Optional] Maximum instances to launch
+            InstanceType='t2.micro',
+            KeyName=key_pem,
+            SecurityGroups=sg
+        )
+        print(f'ec2 Created Successfully')
+    except ClientError as e:
+        print(e)
+        return False
+    return True
+
+
 if __name__ == "__main__":
-    queue_name = cfg.Q_NAME
+    queue_name = Q_NAME
 
     region = 'eu-central-1'
-    blocks_bucket = cfg.BUCKET_NAME
+    blocks_bucket = BUCKET_NAME
 
-    transaction_table_name = cfg.TRANSACTION_TABLE
+    transaction_table_name = TRANSACTION_TABLE
     transaction_keys = [('transaction_id', 'HASH', 'S')]  # (COL_NAME, KEY_TYPE, ATTR_TYPE)
 
-    user_table_name = cfg.USER_TABLE
+    user_table_name = USER_TABLE
     user_keys = [('user_email', 'HASH', 'S')]  # (COL_NAME, KEY_TYPE, ATTR_TYPE)
 
     create_db(user_table_name, user_keys)
     create_db(transaction_table_name, transaction_keys)
     create_bucket(blocks_bucket, region)
     create_sqs(queue_name)
+    create_instances(num_instances=NUM_INSTANCES, image_id=IMAGE_ID, key_pem=KEY, sg=SECURITY_GROUPS)
